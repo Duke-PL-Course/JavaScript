a9259b8a691b5bda160f43a38ad19e9301988636
Yang Su <suyang1+services@gmail.com>
2013-03-19 16:59:04 -0400

    review solutions

--- review/review.js
+++ review/review.js
@@ -1,3 +1,12 @@
 (function () {
-  // ...
+  var lis = document.getElementsByTagName('li');
+  var genClick = function(index) {
+    return function (e) {
+      alert(index);
+    };
+  }
+  for (var i = 0, len = lis.length; i < len; i += 1) {
+    var li = lis[i];
+    li.onclick = genClick(i);
+  }
 }());
